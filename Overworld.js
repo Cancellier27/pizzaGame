8d7de38618f64bb4d53724eb70327564a076476a
Filipe Costa <filipecancelliercosta@gmail.com>
2022-04-28 19:53:37 +0100

    created a gameloop to be used to load the game on the screen in the right sequence and with the most up to date code

--- Overworld.js
+++ Overworld.js
@@ -5,13 +5,27 @@ class Overworld {
     this.ctx = this.canvas.getContext("2d")
   }
 
+  // start the game loog fireing on every single frame loaded
+  // like 60hz it will be called 60 times in a sec
+  startGameLoop() {
+    const step = () => {
+      requestAnimationFrame(() => {
+        step()
+      })
+    }
+    step()
+  }
+
   init() {
+
+    this.startGameLoop()
+
     // draw the map first
     const image = new Image()
     image.onload = () => {
       this.ctx.drawImage(image, 0, 0)
     }
-    image.src = "/images/maps/Demolower.png"
+    image.src = "/images/maps/DemoLower.png"
 
     //place some Game Objects (characters)
     const hero = new GameObject({
@@ -25,10 +39,10 @@ class Overworld {
 
     // draw the hero and npc1 on the screen
 
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx)
-      npc1.sprite.draw(this.ctx)
-    }, 200)
+    // setTimeout(() => {
+    //   hero.sprite.draw(this.ctx)
+    //   npc1.sprite.draw(this.ctx)
+    // }, 200)
 
   }
 }
