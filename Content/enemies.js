7f8d127ce9067e4503341fa5a7160ca8c5a11120
Filipe Costa <filipecancelliercosta@gmail.com>
2022-07-21 19:42:08 +0100

    created enemies file to make enemies dynamic

---
+++ Content/enemies.js
@@ -0,0 +1,30 @@
+window.enemies = {
+  "erio": {
+    name: "Erio",
+    src: "/images/characters/people/erio.png",
+    pizza: {
+      "a": {
+        pizzaId: "s001",
+        maxHp: 50,
+        level: 1,
+      },
+      "b": {
+        pizzaId: "s002",
+        maxHp: 50,
+        level: 1,
+      }
+    }
+  },
+  "beth": {
+    name: "Beth",
+    src: "/images/characters/people/npc1.png",
+    pizza: {
+      "a": {
+        hp: 1,
+        pizzaId: "f001",
+        maxHp: 50,
+        level: 1,
+      },
+    }
+  }
+}
