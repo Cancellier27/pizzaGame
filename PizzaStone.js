44a1b4a101aa935022765c783ed3096752308d12
Filipe Costa <filipecancelliercosta@gmail.com>
2022-10-18 21:10:20 +0100

    added new class pizzaStone

---
+++ PizzaStone.js
@@ -0,0 +1,14 @@
+class PizzaStone extends GameObject {
+  constructor(config) {
+    super(config)
+    this.sprite = new this.sprite({
+      gameObject: this,
+      src: "/images/characters/pizza-stone.png",
+      animations: {
+        "used-down": [ [0,0] ],
+        "unused-down": [ [1,0] ],
+      },
+      currentAnimation: "used-down"
+    })
+  }
+}
