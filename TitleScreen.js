c74cae8dfd261be05e2845df3b81bd4fd6a03295
Filipe Costa <filipecancelliercosta@gmail.com>
2022-10-24 13:39:17 +0100

    creating the TitleScreen class

--- TitleScreen.js
+++ TitleScreen.js
@@ -1,18 +1,44 @@
 class TitleScree {
-  constructor() {
+  constructor({ progress }) {
+    this.progress = progress
+  }
 
+  getOptions(resolve) {
+    return [
+      {
+        label: "New Game",
+        description: "Start a new pizza adventure!",
+        handler: () => {
+          this.close()
+          resolve()
+        }
+      },
+      // maybe insert a Continue here
+    ]
   }
 
   createElement() {
+    this.element = document.createElement("div")
+    this.element.classList.add("TitleScreen")
+    this.element.innerHTML = (`
+    <img class="TitleScreen" src="/imgaes/logo.png" alt="Pizza Legends">
+    `)
 
   }
 
   close() {
-    
+    this.keyboardMenu.end()
+    this.element.remove()
   }
 
-  init() {
-
+  init(container) {
+    return new Promise(resolve => {
+      this.createElement()
+      container.appenChild(this.element)
+      this.keyboardMenu = new keyboardMenu()
+      this.keyboardMenu.init(this.element)
+      this.keyboardMenu.setOptions(this.getOptions(resolve))
+    })
   }
 
 }
